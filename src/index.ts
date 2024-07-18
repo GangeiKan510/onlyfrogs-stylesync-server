155c52de0fa6a321b3b393d8f33675a667381983
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-07-18 20:01:55 +0800

    feat: initialize prisma with remote db

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, { Express, Request, Response } from 'express';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
 import webRouter from './routes/web';
+import prisma from './controllers/db';
 
 const port = process.env.PORT || 3000;
 const app: Express = express();
@@ -18,6 +19,12 @@ app.get('/', (req: Request, res: Response) =>
   res.send('OnlyFrogs StyleSync Server')
 );
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    await prisma.$connect();
+    console.log('Connected to the database successfully.');
+  } catch (error) {
+    console.error('Failed to connect to the database:', error);
+  }
   console.log(`App listening on port: ${port}`);
 });
