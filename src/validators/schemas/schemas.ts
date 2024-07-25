b72f7ae2576e25635023c41af3e327d7597de0e1
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-07-26 00:02:47 +0800

    fix: renamed zod type schema validator

--- src/validators/schemas/schemas.ts
+++ src/validators/schemas/schemas.ts
@@ -8,7 +8,7 @@ export const CreateClothingRequestBodySchema = z.object({
 });
 
 export const UserSchema = z.object({
-  firstName: z.string().nonempty({ message: 'First name is required' }),
-  lastName: z.string().nonempty({ message: 'Last name is required' }),
+  first_name: z.string().nonempty({ message: 'First name is required' }),
+  last_name: z.string().nonempty({ message: 'Last name is required' }),
   email: z.string().email({ message: 'Email must be a valid email address' }),
 });
