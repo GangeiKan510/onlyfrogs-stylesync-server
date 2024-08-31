3bbea1c1dd24aa7c9bb8b8555c556171d02d53fb
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-08-31 15:24:49 +0800

    feat: add axios to dependency and modify upload endpoint to handle background removal

--- src/routes/web/upload.ts
+++ src/routes/web/upload.ts
@@ -7,6 +7,7 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import multer from 'multer';
+import axios from 'axios';
 import firebaseConfig from '../../config/firebase.config';
 
 const router: Router = express.Router();
@@ -42,14 +43,32 @@ router.post('/upload', upload.single('file'), async (req, res) => {
     const downloadURL = await getDownloadURL(snapshot.ref);
 
     console.log('File successfully uploaded.');
+
+    const formData = new URLSearchParams();
+    formData.append('image_url', downloadURL);
+
+    const backgroundRemovalResponse = await axios.post(
+      'http://localhost:4269/remove-background/',
+      formData,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
     return res.send({
-      message: 'File uploaded to Firebase Storage',
+      message: 'File uploaded to Firebase Storage and background removed',
       name: req.file.originalname,
       type: req.file.mimetype,
       downloadURL: downloadURL,
+      backgroundRemovalResponse: backgroundRemovalResponse.data,
     });
   } catch (error: any) {
-    console.error('Error during file upload:', error.message);
+    console.error(
+      'Error during file upload or background removal:',
+      error.message
+    );
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
