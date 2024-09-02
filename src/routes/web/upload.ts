d4ff952ee1b177bfd564d278ef35465a673e0a3f
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-02 13:43:01 +0800

    chore: remove test log

--- src/routes/web/upload.ts
+++ src/routes/web/upload.ts
@@ -40,8 +40,6 @@ router.post('/upload', upload.single('file'), async (req, res) => {
       metadata
     );
 
-    console.log('chore: test discord to github webhook integration');
-
     const downloadURL = await getDownloadURL(snapshot.ref);
 
     console.log('File successfully uploaded.');
