38910f87f533bfd0beb2c2fcb4432c5e12beaf16
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-09-22 02:50:16 +0800

    feat: udpate existing user controller based on schema change and add update user controller for skin tone analyzation

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,14 +3,19 @@ export interface UserProps {
   last_name: string;
   email: string;
   tokens?: number;
-  birth_date?: string;
-  gender?: string;
-  height?: string;
-  skin_tone_classification?: string;
+  birth_date?: string | null;
+  gender?: string | null;
+  height?: number | null;
+  weight?: number | null;
+  skin_tone_classification?: string | null;
+  season?: string | null;
+  sub_season?: string | null;
+  skin_tone_complements?: string[];
+  body_type?: string | null;
   style_preferences?: string[];
-  favorite_color?: string;
-  budget_preferences?: {
-    high: number;
-    low: number;
-  };
+  favorite_colors?: string[];
+  preferred_brands?: string[];
+  budget_min?: number | null;
+  budget_max?: number | null;
+  location?: string | null;
 }
